67a65f8df96c6442d8adafee64d3b0e6ca95a280
PC14 <PC14@INFO5-2807>
2024-02-29 08:43:44 +0100

    fix:  - feature: - ho aggiunto la funzione che stampa in output l'elenco dei voti e dei pesi.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Diego Succi 3G 29/02/2024");
+            Console.WriteLine("inserire il numero di voti");
+            int nvoti = Convert.ToInt32(Console.ReadLine());
+            StampaVotiPesi(voti, pesi, nvoti);
+            
+        }
+        static void StampaVotiPesi(double[] voti, int[] pesi, int nVoti)
+        {
+            Console.WriteLine(" voti  -  pesi ");
+            for (int i = 0; i < voti.Length; i++)
+            {
+                Console.WriteLine(voti[i] + pesi[i]);
+            }
+            
         }
     }
 }
