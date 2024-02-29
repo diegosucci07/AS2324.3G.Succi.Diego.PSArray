c0f101fd7a50b6b61b0011cda201b33a5aec833e
PC14 <PC14@INFO5-2807>
2024-02-29 08:23:58 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AS2324._3G.Succi.Diego.PSArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
